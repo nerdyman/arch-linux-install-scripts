f6992f3a8beed645de80d531a7e837135e17bb18
nerdyman <averynerdyman@gmail.com>
2019-11-10 16:16:24 +0000

    add plasma-i3 script

---
+++ scripts/desktop-environments/plasma-i3.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function alis_desktop_environment_plasma_minimal {
+	# shellcheck disable=SC2155
+	local _script_name="$(basename "${BASH_SOURCE[0]}")"
+	echo -e "[${_script_name}]\\n"
+
+	local _default_user
+	_default_user=$(whoami)
+
+	if [[ "$1" == "help" ]]; then
+		echo -e "=> Plasma with i3wm\\n"
+		echo "=> You can specify the following:"
+		echo "==> User to run install as (default: ${_default_user})"
+		echo "[${_script_name}]"
+		exit 125
+	fi
+
+	local _user=${1:-$_default_user}
+
+	local _install_targets="ark flameshot i3-gaps-rounded kde-gtk-config nitrogen plasma-desktop redshift rofi"
+
+	echo "=> Install targets: ${_install_targets}"
+
+	# shellcheck disable=SC2086
+	sudo -H -u $_user yay -S --needed --noconfirm $_install_targets
+
+	echo -e "\\n[${_script_name}] Done\\n"
+}
+
+alis_desktop_environment_plasma_minimal "$1"
