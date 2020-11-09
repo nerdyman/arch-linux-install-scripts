54ad343d3bd07f76304084bbec71b9e8e6aa70fa
nerdyman <averynerdyman@gmail.com>
2020-11-09 18:33:48 +0000

    run yay as normal user in qt install script

--- scripts/base/qt-install.bash
+++ scripts/base/qt-install.bash
@@ -6,7 +6,7 @@ function alis_qt_install {
 	echo -e "[${_script_name}]\n"
 
 	echo "=> Installing packages"
-	yay -S --needed --noconfirm qt5-base qt5-styleplugins qt5-wayland
+	sudo -H -u "$_user" yay -S --needed --noconfirm qt5-base qt5-styleplugins qt5-wayland
 
 	echo -e "\n[${_script_name}] Done\n"
 }
